99930006fcb6f484dacafed37144cb877053e6b7
eureka-cpu <github.eureka@gmail.com>
2025-03-17 23:54:01 -0700

    handle workdir

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::str::FromStr;
+use std::{env::current_dir, str::FromStr};
 
 use crate::{
     cli::{NixWatch, Options, Parser},
@@ -15,15 +15,17 @@ fn main() -> anyhow::Result<()> {
         exec,
         shell,
         ignore: _,
-        workdir: _,
+        workdir,
         print_build_logs: _,
     } = opts;
 
     env_logger::builder()
         .filter_level(log::LevelFilter::from_str(&flags.log_level)?)
         .init();
+
     let _nix_commands = get_nix_commands(&exec);
     let _shell_commands = get_shell_commands(&shell);
+    let _workdir = workdir.map_or_else(current_dir, Ok)?;
 
     Ok(())
 }
